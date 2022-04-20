5cb1e4dd9a924246a1c1d1b512f3210cdb983f6a
Benin Clint <beninclint@gmail.com>
2022-04-20 22:25:54 +0530

    End of Sectgion 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,30 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
-        }
-        [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
-        {
-            var users = _context.Users.ToList();
-            return users;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
-        [Authorize]
-         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var user = _context.Users.Find(id);
-            return user;
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServicees(this IServiceCollection services, IConfiguration config)
         {
              services.AddScoped<ITokenService,TokenService>();
+             services.AddScoped<IUserRepository,UserRepository>();
+             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(option =>
             {
                 option.UseSqlite(config.GetConnectionString("DefaultConnection"));
